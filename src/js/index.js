30d0b3bdc131a3a500f9d40d59b370fcf3ef0d78
Julia Retuerta <elgrankeko@hotmail.com>
2024-06-05 18:57:56 +0200

    update

--- src/js/index.js
+++ src/js/index.js
@@ -1,2 +1,26 @@
 // El styles lo importamos aquí, ya se carga después al compilar todo
 import '../scss/styles.scss';
+
+//Para crear intervalos
+
+// let counter = 0;
+// let intervalo;
+
+// function crearIntervalo() {
+//   if (intervalo) {
+//     destruirIntervalo();
+//     counter = 0;
+//   }
+//   intervalo = setInterval(() => {
+//     counter++;
+//     console.log(counter);
+//   }, 1000);
+// }
+
+// function destruirIntervalo() {
+//   clearInterval(intervalo);
+// }
+
+//ponemos crearIntervalo() para empezar el intervalo
+
+//ponemos destruirIntervalo() para quitar el intervalo
