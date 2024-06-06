4c87a7cbca17440aa416fbc802365f10882ca10a
Julia Retuerta <elgrankeko@hotmail.com>
2024-06-06 19:05:39 +0200

    update

--- src/js/index.js
+++ src/js/index.js
@@ -1,7 +1,7 @@
 // El styles lo importamos aquí, ya se carga después al compilar todo
 import '../scss/styles.scss';
 
-//Para crear intervalos
+//PARA CRAR INTERVALOS
 
 // let counter = 0;
 // let intervalo;
@@ -24,3 +24,53 @@ import '../scss/styles.scss';
 //ponemos crearIntervalo() para empezar el intervalo
 
 //ponemos destruirIntervalo() para quitar el intervalo
+
+//////////////////////////////////////////////////////////////////////////////////
+
+//ESTO ES PARA HACER UN INPUT QUE CUANDO SE ESCRIBA ALGO SE HABILITE EL BOTON
+
+// BUSQUEDA DE ELEMENTOS
+// DEL DOM CON SELECTORES
+const inputEntrada = document.getElementById('entrada');
+const botonEnviar = document.getElementById('send');
+
+// DECLARACION DE FUNCIONES
+function checarInputVacio(valor) {
+  const entradaInputNoVacia = valor !== '';
+
+  if (entradaInputNoVacia) {
+    botonEnviar.disabled = false;
+  } else {
+    botonEnviar.disabled = true;
+  }
+}
+
+function botonPulsado() {
+  console.log('has clickado el boton');
+  console.log('botonPulsado', inputEntrada.value);
+  limpiarInput();
+}
+
+function limpiarInput() {
+  const prf = document.createElement('p');
+
+  prf.textContent = inputEntrada.value;
+  document.querySelector('body').appendChild(prf);
+  inputEntrada.value = '';
+  checarInputVacio(inputEntrada.value);
+}
+
+// INVOCACION DE FUNCION PARA
+// DESHABILITAR EL BOTON DE ENVIAR Y BORRAR
+checarInputVacio(inputEntrada.value);
+
+// CAPTURA DEL EVENTO DE TECLADO
+// PARA LEER EL VALOR DEL INPUT
+function manejadorDelKeyUp(evento) {
+  console.log(evento);
+  console.log(evento.target.value);
+  checarInputVacio(evento.target.value);
+}
+
+inputEntrada.addEventListener('keyup', manejadorDelKeyUp);
+botonEnviar.addEventListener('click', limpiarInput);
